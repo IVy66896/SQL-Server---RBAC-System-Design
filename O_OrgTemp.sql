472c62a143510a40d7d3b1c32ecbff459465d259
peter0817 <59242083+peter0817@users.noreply.github.com>
2020-02-09 04:16:10 +0000

    Create O_OrgTemp.sql

---
+++ O_OrgTemp.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[O_OrgTemp] (
+	[OwnOrgName] nvarchar(255) COLLATE Chinese_Taiwan_Stroke_CI_AS, 
+	[OwnOrgID] int, 
+	[AgencyCode] nvarchar(255) COLLATE Chinese_Taiwan_Stroke_CI_AS, 
+	[OrgAgencyName] nvarchar(255) COLLATE Chinese_Taiwan_Stroke_CI_AS, 
+	[InoNote] nvarchar(255) COLLATE Chinese_Taiwan_Stroke_CI_AS, 
+	[IsClose] nvarchar(255) COLLATE Chinese_Taiwan_Stroke_CI_AS
+)
